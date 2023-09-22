d6aec57b3eb680304680cdba9a780bb797d67abe
Pavel Denisov <denisov@itopcase.ru>
2023-09-22 18:09:08 +0300

    just start

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,18 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const pathKeys = path.split(".");
 
+  return (obj) => {
+    console.log("___________________________________");
+    let currentObj = obj;
+    pathKeys.map((pathKey) => {
+      if (pathKey in obj) {
+        console.log("here:", pathKey);
+        currentObj = obj[pathKey];
+      }
+    });
+    console.log("currentObj:", currentObj);
+    return currentObj;
+  };
 }

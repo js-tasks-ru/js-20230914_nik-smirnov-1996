941af1e85337a6aff5a9e3a6ce4a019b361f4e03
denisov <denisov@itopcase.ru>
2023-09-26 22:19:04 +0300

    start

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,15 @@
 export default class ColumnChart {
+    constructor(config) {
+        this.config = config;
+        this.element = document.createElement(`div`)
+        this.element.innerHTML = `<div>test</div>`
+    }
+
+    // element = `<div>test</div>`;    
+
+    // element() {
+    //     const test = document.createElement('div')
+    //     test.innerHTML = `<div>test</div>`;
+    // }
 
 }

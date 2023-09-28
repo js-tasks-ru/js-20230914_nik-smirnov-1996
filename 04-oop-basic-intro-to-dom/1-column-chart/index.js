1da719571257846b022a47741165646af64e701e
Pavel Denisov <denisov@itopcase.ru>
2023-09-28 15:48:11 +0300

    fourth task

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,25 +1,28 @@
 export default class ColumnChart {
+  chartHeight = 50;
+
   constructor(config = {}) {
     this.config = { ...config };
+    this.element = this.createElement();
   }
 
-  title(config) {
-    const link = `<a href=${config?.link} class="column-chart__link">View all</a>`;
-
-    return config?.link ? `${config?.label}` + link : `${config?.label}`; 
+  title() {
+    const link = `<a href=${this.config?.link} class="column-chart__link">View all</a>`;
+    return this.config?.link
+      ? `${this.config?.label}` + link
+      : `${this.config?.label}`;
   }
 
-  createElement(config) {
-
-    const maxValue = Math.max(...config?.data);
-
+  createElement() {
     let element = document.createElement(`div`);
     element.className = "column-chart";
-    element.style = "--chart-height: 50";
+    if (!this.config?.data || this.config?.data.length === 0) {
+      element.classList.add("column-chart_loading");
+    }
 
     let title = document.createElement("div");
     title.className = "column-chart__title";
-    title.innerHTML = this.title(config);
+    title.innerHTML = this.title();
 
     let container = document.createElement("div");
     container.className = "column-chart__container";
@@ -27,21 +30,28 @@ export default class ColumnChart {
     let containerHeader = document.createElement("div");
     containerHeader.className = "column-chart__header";
     containerHeader.dataset.element = "header";
-    containerHeader.innerHTML = config?.value;
+    containerHeader.innerHTML = this.config?.formatHeading
+      ? this.config?.formatHeading(this.config?.value)
+      : this.config?.value;
 
     let containerBody = document.createElement("div");
     containerBody.className = "column-chart__chart";
     containerBody.dataset.element = "body";
 
     let containerBodyElement = document.createElement("div");
-    
-    config?.data.forEach((element, index) => {
-        let cloneElement = containerBodyElement.cloneNode();
-        cloneElement.key = index;
-        cloneElement.style = `--value: ${(50 * element) / maxValue}`;
-        cloneElement.dataset.tooltip = `${Math.round((element / maxValue) * 100)}%`;
-        containerBody.appendChild(cloneElement);
-    })
+
+    this.config?.data?.forEach((element, index) => {
+      let cloneElement = containerBodyElement.cloneNode();
+      const maxValue = Math.max(...this.config?.data);
+      cloneElement.key = index;
+      cloneElement.style = `--value: ${Math.floor(
+        (this.chartHeight * element) / maxValue
+      )}`;
+      cloneElement.dataset.tooltip = `${((element / maxValue) * 100).toFixed(
+        0
+      )}%`;
+      containerBody.appendChild(cloneElement);
+    });
 
     container.appendChild(containerHeader);
     container.appendChild(containerBody);
@@ -52,7 +62,15 @@ export default class ColumnChart {
     return element;
   }
 
-  get element() {
-    return this.createElement(this.config);
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
+
+  update(value) {
+    this.config.data = value;
   }
 }

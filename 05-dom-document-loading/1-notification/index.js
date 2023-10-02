f19cad56e43c216a2d613396da82708ca97ce0f8
Pavel Denisov <denisov@itopcase.ru>
2023-10-02 21:00:56 +0300

    made first task

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,56 @@
 export default class NotificationMessage {
+  static isModalShowed = false;
 
+  constructor(...config) {
+    this.message = config[0];
+    this.duration = config[1]?.duration;
+    this.type = config[1]?.type;
+    this.element = this.createModalElement();
+  }
+
+  addModal() {
+    return `
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+            ${
+              this.type
+                ? `<div class="notification-header">${this?.type}</div>`
+                : ""
+            }
+            ${
+              this.message
+                ? `<div class="notification-body"> ${this.message} </div>`
+                : ""
+            }
+        </div>
+    `;
+  }
+
+  remove() {
+    NotificationMessage.isModalShowed = false;
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
+
+  createModalElement() {
+    let element = document.createElement("div");
+    element.className = `notification ${this.type ?? ""}`;
+    element.style = `--value: ${this.duration / 1000}s`;
+    element.innerHTML = this.addModal();
+    return element;
+  }
+
+  show(parent = document.body) {
+    if (NotificationMessage.isModalShowed) {
+      return;
+    }
+    NotificationMessage.isModalShowed = true;
+    parent.append(this.element);
+    setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,56 @@
 export default class SortableTable {
   constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig;
+    this.data = data;
+    this.element = this.createTableElement();
+  }
 
+  /* <div data-element="header" class="sortable-table__header sortable-table__row">
+      <div class="sortable-table__cell" data-id="images" data-sortable="false" data-order="asc">
+        <span>Image</span>
+      </div>
+      <div class="sortable-table__cell" data-id="title" data-sortable="true" data-order="asc">
+        <span>Name</span>
+        <span data-element="arrow" class="sortable-table__sort-arrow">
+          <span class="sort-arrow"></span>
+        </span>
+      </div>
+      <div class="sortable-table__cell" data-id="quantity" data-sortable="true" data-order="asc">
+        <span>Quantity</span>
+      </div>
+      <div class="sortable-table__cell" data-id="price" data-sortable="true" data-order="asc">
+        <span>Price</span>
+      </div>
+      <div class="sortable-table__cell" data-id="sales" data-sortable="true" data-order="asc">
+        <span>Sales</span>
+      </div>sortable
+    </div> */
+
+  addTableHeader() {
+    return `
+      <div data-element="header" class="sortable-table__header sortable-table__row">
+        ${this.headerConfig.forEach((headerElement) => {
+          `<div class="sortable-table__cell" 
+          data-id=${headerElement.id} 
+          data-sortable=${headerElement.sortable ?? "false"} 
+          data-order=${headerElement.sortDirection ?? 'asc'}>
+            <span>${headerElement.title}</span>
+          </div>`
+        })}
+      <div>
+    `;
+  }
+
+  createTableElement() {
+    let element = document.createElement("div");
+    element.className = `sortable-table`;
+    element.innerHTML = `${this.addTableHeader()}`;
+    return element;
+  }
+
+  sort(fieldValue, orderValue) {
+    document.body.append(this.element);
+    console.log("fieldValue:", fieldValue);
+    console.log("orderValue:", orderValue);
   }
 }
-

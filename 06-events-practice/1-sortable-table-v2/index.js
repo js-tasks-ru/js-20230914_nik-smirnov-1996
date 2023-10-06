aee1233c9872c18f198f92d453bba90dcb0e2176
Pavel Denisov <denisov@itopcase.ru>
2023-10-06 18:31:09 +0300

    start

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,13 @@
-export default class SortableTable {
-  constructor(headersConfig, {
-    data = [],
-    sorted = {}
-  } = {}) {
+import { SortableTable as SortableTableParent } from "../../05-dom-document-loading/2-sortable-table-v1/index.js";
 
+export default class SortableTable extends SortableTableParent {
+  #isSortLocally;
+
+  constructor(headersConfig, { data = [], sorted = {} } = {}, isSortLocally = true) {
+    super(headersConfig, data);
+    this.#isSortLocally = isSortLocally;
+    this.sort(sorted?.id, sorted?.oreder);
+    console.log("here", super.subElements);
   }
+
 }
